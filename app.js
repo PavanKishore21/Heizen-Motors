a7f7fce2e082c6a4331996d6aaf5f9059a103261
PavanKishore21 <pavankishoreramavath@gmail.com>
2021-10-29 20:15:33 +0530

    minor

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static('assets'))
 app.use('/css',express.static(__dirname + 'assets/css'))
@@ -21,5 +21,5 @@ app.get('/gallery', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
